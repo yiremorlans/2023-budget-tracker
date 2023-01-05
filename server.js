79940fae0d0abcd661401d51edfe0501f4b29e4b
Yire Morlans <yire.m@icloud.com>
2023-01-05 17:17:27 -0500

    import db connection to server

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const app = express();
 const PORT = process.env.PORT;
 const mongoose = require("mongoose");
 
+const connectDB = require("./config/database");
+require("dotenv").config();
+
+connectDB();
 // MIDDLEWARE
 app.set("view engine", "ejs");
 app.set(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT || process.env.PORT, () =>
+app.listen(process.env.PORT || PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
